9eecfade2545ad157b54944487754f9225468f8a
Josep Alsina <josepalsinasio@gmail.com>
2021-03-08 14:18:13 +0700

    0.2.0

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
 // ==UserScript==
 // @name         UOC Login Automatic
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Automatització login
 // @author       You
 // @require		http://code.jquery.com/jquery-2.1.0.min.js
-// @match       https://www.uoc.edu/*
+// @match       https://*.uoc.edu/*
 // // @match        https://www.uoc.edu/portal/*.html
 // @run-at  document-end
 // @grant        none
 // ==/UserScript==
 
-(() =>{
+(() => {
     'use strict';
 
     const iconTagGoToLoginPage = '.icon--campus';
     const loginPage = {
         usernameHandler: '#username',
-        submitLoginHandler: '#submitButton'
+        passwordHandler: '#password',
+        submitLoginHandler: '#fm1'
     };
 
-    var curPages, iPage;
-
-    function doCourse(){
-        curPages = $('#LoadFrame > div > div.pagination > div').children().length;
-        iPage = 1;
-        var autoclickInterval = setInterval( ()=>{
-            $('#LoadFrame > div > div.pagination > span.button-page.right > a > img.web-arrow').click();
-            console.log(iPage, curPages);
-            if (++iPage >= curPages){
-                clearInterval(autoclickInterval);
-            }
-        } , 3000);
-    };
-
-    redirectToLoginPage: ()=>{
-
-    };
-
-    handleLoginPage: ()=>{
-        if ($(loginPage.usernameHandler) && $(loginPage.submitLoginHandler)){
-            console.log('found username and submit input');
-            $(loginPage.submitLoginHandler).click();
+    function redirectToLoginPage() {
+        if ($(iconTagGoToLoginPage)) {
+            $(iconTagGoToLoginPage).click();
         }
     };
 
+    function handleLoginPage() {
+        if ($(loginPage.usernameHandler) && $(loginPage.submitLoginHandler)) {
+
+            setTimeout(() => {
+                $(loginPage.submitLoginHandler).submit();
+            }, 3000);
+        }
+    }
+
+    const thisUrl = document.location.href;
+
+    if (thisUrl.includes('portal') && thisUrl.includes('index')) {
+        redirectToLoginPage();
+    }
+
+    if (thisUrl.includes('login')) {
+        handleLoginPage();
+    }
 
 })();
